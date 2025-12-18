319e2f224bfaac6960ae3a0a74fd82d137932924
Sergio Arenas <160001076+SergioArenas98@users.noreply.github.com>
2025-12-18 22:50:55 +0000

    railway

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos del build de Angular
-app.use(express.static(path.join(__dirname, 'dist/paleohumans-front')));
+const DIST_FOLDER = path.join(__dirname, 'dist', 'paleohumans-front');
 
-// Catch-all usando app.use en vez de app.get
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'dist/paleohumans-front/index.html'));
-});
+app.use(express.static(DIST_FOLDER));
+app.get('*', (req, res) => res.sendFile(path.join(DIST_FOLDER, 'index.html')));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
