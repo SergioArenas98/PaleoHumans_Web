f7183ed864b1d40dc39d6c4164ce8f8c3cdb18ec
Sergio Arenas <160001076+SergioArenas98@users.noreply.github.com>
2025-12-18 22:45:33 +0000

    railway

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos del build de Angular
 app.use(express.static(path.join(__dirname, 'dist/paleohumans-front')));
 
-// Redirigir todas las rutas a index.html para Angular Router
-app.get('*', (req, res) => {
+// Catch-all usando app.use en vez de app.get
+app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'dist/paleohumans-front/index.html'));
 });
 
